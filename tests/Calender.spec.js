892db02568ec4f6a7d332e8ec17c8785641c05b0
rajnish17 <rajnishdt@gmail.com>
2026-02-20 16:36:50 +0530

    Initial commit

---
+++ tests/Calender.spec.js
@@ -0,0 +1,12 @@
+const {test, expect} = require('@playwright/test')
+
+
+test ('Calender validations', async ({browser}) =>
+{
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+
+
+});
