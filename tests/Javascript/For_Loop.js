c82c4a15b9daadc23386425cb19986c9e454fcc2
rajnish17 <rajnishdt@gmail.com>
2026-03-11 23:05:12 +0530

    Update For_Loop.js and add Arrays.js

--- tests/Javascript/For_Loop.js
+++ tests/Javascript/For_Loop.js
@@ -12,3 +12,30 @@ for (i=1; i<=100; i++)
     if(i%3 == "0" && i%5 == 0)
     console.log(i)
 }
+
+console.log ("******************************");
+
+// from 1 to 100, give me common multipls of 3 & 5 & take only first 2 numbers
+
+let n = 0
+for (i =1; i<=100; i++)
+{
+    
+    if( i%3 == 0 && i%5 == 0)
+    {
+        n++
+        console.log(i)
+        if(n==2)
+        break;
+    }
+}
+
+console.log ("******************************");
+
+let required = true;
+while(required)
+{
+    console.log(required)
+    required = false;
+
+}
